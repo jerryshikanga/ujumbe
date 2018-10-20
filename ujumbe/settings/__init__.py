1c4aa159f754132d9948551dc55d5533e9e25ffd
Jerry Shikanga <jerryshikanga@gmail.com>
2018-10-21 01:12:06 +0300

    Merge check value eists into one setting

--- ujumbe/settings/__init__.py
+++ ujumbe/settings/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 def get_boolean_value_from_config(config_value : str=None):
     """Checks if value is 1 returns true, if zero returns false else raises value error"""
     if config_value is None: raise ValueError
@@ -9,3 +11,10 @@ def get_boolean_value_from_config(config_value : str=None):
         return False
     else:
         raise ValueError("Unexpected value. Only are 0 and 1 are valids")
+
+
+def check_if_config_dict_exists(value_key : str = None, config_dict : dict = None):
+    if config_dict is None or value_key is None:
+        message = "No {} setting found".format(value_key)
+        logging.error(message)
+        raise KeyError(message)
