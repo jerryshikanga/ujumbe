1fab491809fb66c031a87b557e96ec19b770307c
Jerry Shikanga <jerryshikanga@gmail.com>
2018-11-05 23:41:50 +0300

    Use django-environ for settings

--- ujumbe/settings/custom.py
+++ ujumbe/settings/custom.py
@@ -1,23 +1,20 @@
 from ujumbe.settings.base import *
 from ujumbe.settings import check_if_config_dict_exists
 
-check_if_config_dict_exists(value_key="AFRICASTALKING", config_dict=config)
-AFRICASTALKING_USERNAME = config['AFRICASTALKING']['username']
-AFRICASTALKING_API_KEY = config['AFRICASTALKING']['api_key']
+AFRICASTALKING_USERNAME = env("AFRICASTALKING_USERNAME")
+AFRICASTALKING_API_KEY = env("AFRICASTALKING_API_KEY")
 
-check_if_config_dict_exists(value_key="EMAIL", config_dict=config)
-EMAIL_HOST = config["EMAIL"]["host"]
-EMAIL_PORT = config["EMAIL"]["port"]
-EMAIL_HOST_USER = config["EMAIL"]["user"]
-EMAIL_HOST_PASSWORD = config["EMAIL"]["password"]
-EMAIL_USE_TLS = get_boolean_value_from_config(config["EMAIL"]["tls"])
-EMAIL_USE_SSL = get_boolean_value_from_config(config["EMAIL"]["ssl"])
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_USE_SSL = env("EMAIL_USE_SSL")
 
 DEFAULT_FROM_EMAIL = "admin@admin.com"
 SERVER_EMAIL = 'server@admin.com'
 
-check_if_config_dict_exists(value_key="OPENWEATHER", config_dict=config)
-OPEN_WEATHER_APP_ID = config["OPEN_WEATHER"]["app_id"]
+OPEN_WEATHER_APP_ID = env("OPEN_WEATHER_APPID")
 
 LOGGING = {
     'version': 1,
@@ -37,13 +34,10 @@ LOGGING = {
 }
 
 
-check_if_config_dict_exists(value_key="SUPPORT", config_dict=config)
-CUSTOMER_SUPPORT_PHONE = config["SUPPORT"]["phone"]
-CUSTOMER_SUPPORT_EMAIL = config["SUPPORT"]["email"]
+CUSTOMER_SUPPORT_PHONE = env("CUSTOMER_SUPPORT_EMAIL")
+CUSTOMER_SUPPORT_EMAIL = env("CUSTOMER_SUPPORT_TELEPHONE")
 
-
-check_if_config_dict_exists(value_key="NETATMO", config_dict=config)
-NETATMO_USERNAME = config["NETATMO"]["username"]
-NETATMO_PASSWORD = config["NETATMO"]["password"]
-NETATMO_CLIENT_ID = config["NETATMO"]["client_id"]
-NETATMO_CLIENT_SECRET = config["NETATMO"]["client_secret"]
+NETATMO_USERNAME = env("NETATMO_USERNAME")
+NETATMO_PASSWORD = env("NETATMO_PASSWORD")
+NETATMO_CLIENT_ID = env("NETATMO_CLIENT_ID")
+NETATMO_CLIENT_SECRET = env("NETATMO_CLIENT_SECRET")
