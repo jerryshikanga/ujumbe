ccf51fd4f13b2b610058211fa4f2c387cee862ec
Jerry Shikanga <jerryshikanga@gmail.com>
2018-11-06 00:13:27 +0300

    Update static settings

--- ujumbe/settings/base.py
+++ ujumbe/settings/base.py
@@ -137,4 +137,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://shikanga.com/ujumbe/static/'
+STATIC_ROOT = "/var/www/html/ujumbe/static/"
