ae642121f0593ee7f21d024a87a5790a8b241e41
Jerry Shikanga <jerryshikanga@gmail.com>
2018-11-13 13:33:31 +0300

    Subscription fixes

--- ujumbe/apps/profiles/models.py
+++ ujumbe/apps/profiles/models.py
@@ -75,7 +75,7 @@ class SubscriptionManager(models.Manager):
 
     def due(self):
         time_now = datetime.datetime.now()
-        return Subscription.objects.active().filter(last_send__gte=time_now-F("frequency"))
+        return Subscription.objects.active().filter(last_send__lte=time_now-F("frequency"))
 
     def active(self):
         return super(SubscriptionManager, self).get_queryset().filter(active=True)
