c64a3c3c9ac7a9bc2df4ac6614933ddbfa374b8f
Jerry Shikanga <jerryshikanga@gmail.com>
2018-12-02 13:17:46 +0300

    Update dir settings

--- ujumbe/settings/base.py
+++ ujumbe/settings/base.py
@@ -15,6 +15,7 @@ import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.join(BASE_DIR, "..")
 
 env = environ.Env(
     # set casting, default value
@@ -72,7 +73,7 @@ ROOT_URLCONF = 'ujumbe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(PROJECT_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -139,4 +140,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = 'http://shikanga.com/static/ujumbe/'
-STATIC_ROOT = "/var/www/html/static/ujumbe/"
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
