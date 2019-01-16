8b04a14ed07ddd155deae2d5eab5659d5d968f00
Jerry Shikanga <jerryshikanga@gmail.com>
2019-01-17 01:21:20 +0300

    Add telerivet to urls

--- ujumbe/apps/africastalking/urls.py
+++ ujumbe/apps/africastalking/urls.py
@@ -4,6 +4,7 @@ from ujumbe.apps.africastalking import views
 app_name = "africastalking"
 
 urlpatterns = [
+    path("telerivet/callback/", views.TelerivetWebhookView.as_view(), name="telerivet_webhook_view"),
     path("sms/incoming/", views.ATIncomingMessageCallbackView.as_view(), name="incoming_message_callback"),
     path("sms/outgoing/", views.AtOutgoingSMSCallback.as_view(), name="outgoing_message_callback"),
     path("ussd/callback/", views.AtUssdcallbackView.as_view(), name="at_ussd_callback_view"),
