cf521f1eb6f7a232caeb2a586b1538ec98ffa19d
Jerry Shikanga <jerryshikanga@gmail.com>
2019-01-19 00:34:14 +0300

    Static and media urls

--- ujumbe/settings/base.py
+++ ujumbe/settings/base.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ujumbe/static/'
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/ujumbe/media/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
