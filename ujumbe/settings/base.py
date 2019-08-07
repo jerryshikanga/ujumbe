acac1fdececb9cfd365d33dd482b640ae0c4a32e
Jerry Shikanga <jerryshikanga@gmail.com>
2019-08-08 02:36:02 +0300

    Default value for secret key

--- ujumbe/settings/base.py
+++ ujumbe/settings/base.py
@@ -39,7 +39,7 @@ if DO_SENTRY_LOGGING:
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="secretkey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=False)
